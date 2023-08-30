2a26a5537aef2d8d54e1247a63be315b32c55eb5
Daniel Mack <daniel.mack@holoplot.com>
2023-08-30 15:16:03 +0200

    net: add device internal statistics ethtool
    These statistics can now be read through ethtool.

--- modules/net/ethtool.c
+++ modules/net/ethtool.c
@@ -12,6 +12,93 @@
 #include <ravenna/version.h>
 #include "main.h"
 
+static const char ra_net_gstrings_stats[][ETH_GSTRING_LEN] = {
+	"udp_throttled_packets",
+	"rx_packets_parsed",
+	"rx_queue_errors",
+	"rx_checksum_errors",
+	"rx_stream_packets_dropped",
+	"rx_stream_packets",
+	"rx_legacy_packets",
+	"tx_stream_packets",
+	"tx_legacy_packets",
+	"tx_stream_packets_lost",
+};
+
+struct ra_net_stats {
+	u64 udp_throttled_packets;
+	u64 rx_packets_parsed;
+	u64 rx_queue_errors;
+	u64 rx_checksum_errors;
+	u64 rx_stream_packets_dropped;
+	u64 rx_stream_packets;
+	u64 rx_legacy_packets;
+	u64 tx_stream_packets;
+	u64 tx_legacy_packets;
+	u64 tx_stream_packets_lost;
+};
+
+static void ra_net_read_stats(struct ra_net_priv *priv,
+			      struct ra_net_stats *stats)
+{
+	BUILD_BUG_ON(ARRAY_SIZE(ra_net_gstrings_stats) != sizeof(*stats) / sizeof(u64));
+
+	stats->udp_throttled_packets =
+		ra_net_ior(priv, RA_NET_PP_CNT_UDP_THROTTLE);
+	stats->rx_packets_parsed =
+		ra_net_ior(priv, RA_NET_PP_CNT_RX_PARSED);
+	stats->rx_queue_errors =
+		ra_net_ior(priv, RA_NET_PP_CNT_RX_QUEUE_ERR);
+	stats->rx_checksum_errors =
+		ra_net_ior(priv, RA_NET_PP_CNT_RX_IP_CHK_ERR);
+	stats->rx_stream_packets_dropped =
+		ra_net_ior(priv, RA_NET_PP_CNT_RX_STREAM_DROP);
+	stats->rx_stream_packets =
+		ra_net_ior(priv, RA_NET_PP_CNT_RX_STREAM);
+	stats->rx_legacy_packets =
+		ra_net_ior(priv, RA_NET_PP_CNT_RX_LEGACY);
+	stats->tx_stream_packets =
+		ra_net_ior(priv, RA_NET_PP_CNT_TX_STREAM);
+	stats->tx_legacy_packets =
+		ra_net_ior(priv, RA_NET_PP_CNT_TX_LEGACY);
+	stats->tx_stream_packets_lost =
+		ra_net_ior(priv, RA_NET_PP_CNT_TX_STREAM_LOST);
+}
+
+static void ra_net_get_strings(struct net_device *netdev, u32 stringset, u8 *buf)
+{
+	switch (stringset) {
+	case ETH_SS_STATS:
+		memcpy(buf, &ra_net_gstrings_stats, sizeof(ra_net_gstrings_stats));
+		break;
+	default:
+		WARN_ON(1);
+		break;
+	}
+}
+
+static int ra_net_get_sset_count(struct net_device *netdev, int sset)
+{
+	switch (sset) {
+	case ETH_SS_STATS:
+		return ARRAY_SIZE(ra_net_gstrings_stats);
+	default:
+		return -EINVAL;
+	}
+}
+
+static void ra_net_get_ethtool_stats(struct net_device *ndev,
+				  struct ethtool_stats *estats, u64 *data)
+{
+	struct ra_net_priv *priv = netdev_priv(ndev);
+	struct ra_net_stats stats;
+
+	ra_net_read_stats(priv, &stats);
+
+	memcpy(data, &stats, sizeof(stats));
+}
+
+
 static void ra_net_ethtool_getdrvinfo(struct net_device *ndev,
 				      struct ethtool_drvinfo *info)
 {
@@ -81,6 +168,9 @@ static int ra_net_ethtool_get_module_eeprom(struct net_device *ndev,
 
 const struct ethtool_ops ra_net_ethtool_ops = {
 	.get_drvinfo		= ra_net_ethtool_getdrvinfo,
+	.get_strings		= ra_net_get_strings,
+	.get_sset_count		= ra_net_get_sset_count,
+	.get_ethtool_stats	= ra_net_get_ethtool_stats,
 	.get_msglevel		= ra_net_ethtool_getmsglevel,
 	.set_msglevel		= ra_net_ethtool_setmsglevel,
 	.get_link		= ethtool_op_get_link,
