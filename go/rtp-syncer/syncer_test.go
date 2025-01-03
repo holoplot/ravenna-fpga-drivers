c23ee1c34f95caad4c1f3319efb3d99619825eb2
Daniel Mack <daniel.mack@holoplot.com>
2025-01-03 08:00:34 +0100

    go: refine RTP syncer
    The algorithm to sync the local device to the PTP network requires a
    quantization of the PTP time to a sample counter.
    
    The current implementation does a simple integer division, which can lead
    to rounding errors when the product of the PTP time and the sample rate is
    close to a multiple of `nanoSecondsPerSecond`. This can lead to the local
    device updating the RTP offset frequently, which causes audio artifacts.
    Hence, the implementation allowed for a deviation of ±1 sample.
    
    This, however, is not sufficient for applications requiring higher time
    precision.
    
    This patch introduces an improved method by resolving the sample counter to
    sub-samples. The number of sub-samples per sample is currently set to 8,
    and the allowed deviation is set to a quarter sample. This refined
    measurement enables sample-accurate comparisons during periodic checks
    while avoiding unnecessary updates caused by integer divisions.
    
    Only if the offset changes by more than a quarter sample, the RTP offset
    is adjusted in the FPGA.
    
    Also add some comments and a test suite.

---
+++ go/rtp-syncer/syncer_test.go
@@ -0,0 +1,115 @@
+package syncer
+
+import (
+	"math/big"
+	"testing"
+	"time"
+)
+
+func Test_subSamples_diff(t *testing.T) {
+	tests := []struct {
+		name string
+		a, b subSamples
+		want int64
+	}{
+		{
+			name: "null",
+			a:    0,
+			b:    0,
+			want: 0,
+		},
+		{
+			name: "test1",
+			a:    4003,
+			b:    0,
+			want: 4003,
+		},
+		{
+			name: "test2",
+			a:    1000,
+			b:    1000,
+			want: 0,
+		},
+		{
+			name: "test3",
+			a:    4003,
+			b:    4004,
+			want: -1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.a.diff(tt.b); got != tt.want {
+				t.Errorf("subSamples.diff() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_subSamples_toOffset(t *testing.T) {
+	tests := []struct {
+		name string
+		subs subSamples
+		want uint32
+	}{
+		{
+			name: "a",
+			subs: 250 * subSamplesPerSample,
+			want: 250,
+		},
+		{
+			name: "b",
+			subs: 250*subSamplesPerSample + 1,
+			want: 250,
+		},
+		{
+			name: "c",
+			subs: 250*subSamplesPerSample + 2,
+			want: 250,
+		},
+		{
+			name: "d",
+			subs: 250*subSamplesPerSample + 3,
+			want: 250,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.subs.toSamples32(); got != tt.want {
+				t.Errorf("subSamples.toSamples() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestRtpSyncer_ptpTimestampTosubSamples(t *testing.T) {
+	tests := []struct {
+		name         string
+		sampleRate   int
+		ptpTimestamp uint64
+		want         subSamples
+	}{
+		{
+			name:         "null",
+			sampleRate:   48000,
+			ptpTimestamp: 0,
+			want:         0,
+		},
+		{
+			name:         "test1",
+			sampleRate:   48000,
+			ptpTimestamp: uint64(time.Second.Nanoseconds()) + 20833,             // time of 1 sample at 48000 Hz
+			want:         48000*subSamplesPerSample + (subSamplesPerSample - 1), // 48000 samples * 4 + 3 sub samples
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &RtpSyncer{
+				sampleRate: big.NewInt(int64(tt.sampleRate)),
+			}
+			if got := s.ptpTimestampToSubSamples(tt.ptpTimestamp); got != tt.want {
+				t.Errorf("RtpSyncer.ptpTimestampTosubSamples() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
